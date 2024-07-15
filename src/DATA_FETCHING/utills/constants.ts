475b5a2add7dd487eda14da707e64c3fe7b0df3b
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2024-07-16 00:03:22 +0100

    update backend host

--- src/DATA_FETCHING/utills/constants.ts
+++ src/DATA_FETCHING/utills/constants.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = 'http://localhost:8080';
+//export const BASE_URL = 'http://localhost:8080'; 
 
+export const BASE_URL = 'https://zecomedicalapi.onrender.com'; 
 export const BASE_URL_FE = 'http://localhost:3000';
